7195c697de4b7e91dbb86ad03bd2830859bb8a77
Mikhail2107 <mixa.proshin@yandex.ru>
2024-09-14 18:27:51 +0300

    new

--- src/components/widget/Cart/Cart.tsx
+++ src/components/widget/Cart/Cart.tsx
@@ -6,8 +6,9 @@ function Cart() {
 
     return (
         <>
+        <div className="cart-container">
             <h2 className="cart-title">My Cart</h2>
-            <div className="cart-container">
+            <div className="cart-box">
                 <ul className="cart-list">                    
                     <li className="cart-item">
                         <div className="cart-product-info">
@@ -17,12 +18,11 @@ function Cart() {
                                 <span className="cart-product-price">$110</span>
                             </div> 
                             <div className="count-buttons">
-                                <button className="cart-minus-plus">-</button>
+                                <div className="cart-minus-plus button-minus"></div>
                                 <input className='count-input' id='count-input' type="text" value='1 item' placeholder="0"/>                            
-                                <button className="cart-minus-plus">+</button>
-                            </div>                           
-                                
-                                <div className="cart-delete">Delete</div>
+                                <div className="cart-minus-plus button-plus"></div>
+                            </div>  
+                            <div className="cart-delete">Delete</div>
                         </div>
                     </li>
                     <li className="cart-item">
@@ -33,44 +33,11 @@ function Cart() {
                                 <span className="cart-product-price">$110</span>
                             </div> 
                             <div className="count-buttons">
-                                <button className="cart-minus-plus">-</button>
+                                <div className="cart-minus-plus button-minus"></div>
                                 <input className='count-input' id='count-input' type="text" value='1 item' placeholder="0"/>                            
-                                <button className="cart-minus-plus">+</button>
-                            </div>                           
-                                
-                                <div className="cart-delete">Delete</div>
-                        </div>
-                    </li>
-                    <li className="cart-item">
-                        <div className="cart-product-info">
-                        <img className='cart-image' src={Image} alt="product-img" />
-                            <div className="cart-product">                                
-                                <span className="cart-product-title"> Essence Mascara Lash Princess</span>
-                                <span className="cart-product-price">$110</span>
+                                <div className="cart-minus-plus button-plus"></div>
                             </div> 
-                            <div className="count-buttons">
-                                <button className="cart-minus-plus">-</button>
-                                <input className='count-input' id='count-input' type="text" value='1 item' placeholder="0"/>                            
-                                <button className="cart-minus-plus">+</button>
-                            </div>                           
-                                
-                                <div className="cart-delete">Delete</div>
-                        </div>
-                    </li>
-                    <li className="cart-item">
-                        <div className="cart-product-info">
-                        <img className='cart-image' src={Image} alt="product-img" />
-                            <div className="cart-product">                                
-                                <span className="cart-product-title"> Essence Mascara Lash Princess</span>
-                                <span className="cart-product-price">$110</span>
-                            </div> 
-                            <div className="count-buttons">
-                                <button className="cart-minus-plus">-</button>
-                                <input className='count-input' id='count-input' type="text" value='1 item' placeholder="0"/>                            
-                                <button className="cart-minus-plus">+</button>
-                            </div>                           
-                                
-                                <div className="cart-delete">Delete</div>
+                            <div className="cart-delete">Delete</div>
                         </div>
                     </li>
                     <li className="cart-item">
@@ -85,11 +52,35 @@ function Cart() {
                                 <input className='count-input' id='count-input' type="text" value='1 item' placeholder="0"/>                            
                                 <div className="cart-minus-plus button-plus"></div>
                             </div>                           
-                                
-                                <div className="cart-delete">Delete</div>
+                            <div className="cart-delete">Delete</div>
                         </div>
                     </li>
+                    <li className="cart-item">
+                        <div className="cart-product-info">
+                        <img className='cart-image' src={Image} alt="product-img" />
+                            <div className="cart-product">                                
+                                <span className="cart-product-title"> Essence Mascara Lash Princess</span>
+                                <span className="cart-product-price">$110</span>
+                            </div> 
+                            <div className="count-buttons">
+                                {/* <div className="cart-minus-plus button-minus"></div>
+                                <input className='count-input' id='count-input' type="text" value='1 item' placeholder="0"/>                            
+                                <div className="cart-minus-plus button-plus"></div> */}
+                            </div>
+                            <div className="cart-delete button-cart"></div>
+                        </div>
+                    </li>                    
                 </ul>
+            </div>
+            <div className="cart-description">
+                <span className="cart-totalcount">Total count</span>
+                <span className="cart-total-item">3 item</span>
+                <span className="cart-discount">Price without discount</span>
+                <span className="cart-price-discount">$700</span>
+                <span className="cart-discount">Total price</span>
+                <span className="cart-price-discount">$590</span>
+            </div>
+                
             </div>
         </>
     )
