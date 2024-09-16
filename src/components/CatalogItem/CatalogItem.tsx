3df80bc401c408a223e29d78c4d149edcca2cc5d
Mikhail2107 <mixa.proshin@yandex.ru>
2024-09-17 01:05:27 +0300

    remake CatalogItem buttons to buy

--- src/components/CatalogItem/CatalogItem.tsx
+++ src/components/CatalogItem/CatalogItem.tsx
@@ -1,34 +1,58 @@
-import { Link } from 'react-router-dom'
-import Image from '../../assets/image.png'
-import './CatalogItem.css'
+import { Link } from 'react-router-dom';
+import Image from '../../assets/image.png';
+import './CatalogItem.css';
+import { useState } from 'react';
 
 function CatalogItem() {
-    return (
-        <>             
-            <div className="product-item" role="article">
-                <div className="product-imagebox">
-                    <div className="hide-box">
-                        <span className="show-details" tabIndex={5}>Show details</span>
-                    </div>
-                    <Link to='/product/123'>
-                        <img
-                            className="product-image"
-                            title='Essence Mascara'
-                            src={Image} 
-                            alt="Essence Mascara" 
-                            />
-                    </Link>                    
-                </div>
-                <div className="product-card">
-                    <div className="product-info">
-                        <span className="product-name">Essence Mascara</span>
-                        <span className="product-cardprice">$110</span>
-                    </div>
-                    <div className="product-buy" role="button" tabIndex={6}></div>
-                </div>
-            </div>
-        </>
-    );
+  const [count, setCount] = useState(0); 
+
+  const handleIncreaseCount = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  const handleDecreaseCount = () => {
+    if (count > 0) {
+      setCount(prevCount => prevCount - 1);
+    }
+  };
+
+  return (
+    <>
+      <div className="product-item" role="article">
+        <div className="product-imagebox">
+          <div className="hide-box">
+            <span className="show-details" tabIndex={5}>Show details</span>
+          </div>
+          <Link to='/product/123'>
+            <img
+              className="product-image"
+              title='Essence Mascara'
+              src={Image} 
+              alt="Essence Mascara" 
+            />
+          </Link>                    
+        </div>
+
+        <div className="product-card">
+          <div className="product-info">
+            <span className="product-name">Essence Mascara</span>
+            <span className="product-cardprice">$110</span>
+          </div>
+
+          {count === 0 ? (
+            <div className="product-buy" tabIndex={6} onClick={handleIncreaseCount}></div>
+          ) : (
+            <div className="count-buttons">
+              <div className="cart-minus-plus button-minus" onClick={handleDecreaseCount}></div>
+              <span className='count-input' id='count-input'>{count} item</span>                           
+              <div className="cart-minus-plus button-plus" onClick={handleIncreaseCount}></div>
+            </div> 
+          )}
+          
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default CatalogItem
+export default CatalogItem;
