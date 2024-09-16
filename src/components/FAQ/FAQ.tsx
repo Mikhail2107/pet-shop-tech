e39dd45e6bda0e80ac8235208934d9f6aef84f29
Mikhail2107 <mixa.proshin@yandex.ru>
2024-09-17 00:16:41 +0300

    remake FAQ

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -1,72 +1,63 @@
-import { useState } from 'react'; 
+import { useState } from 'react';
 import './FAQ.css';
 
+interface FAQItem {
+  id: number;
+  question: string;
+  answer: string;
+  isOpen: boolean; 
+}
+
 function FAQ() {
-    const [openIndex, setOpenIndex] = useState(null); 
+  const [faqItems, setFaqItems] = useState<FAQItem[]>([
+    { id: 0, 
+      question: 'How can I track the status of my order?', 
+      answer: 'After placing your order, you will receive a confirmation email containing your order number and a tracking link. You can also log in to your account on our website and go to the "My Orders" section to track your delivery status.', 
+      isOpen: false },
+    { id: 1, 
+      question: 'What payment methods do you accept?', 
+      answer: 'We accept various payment methods including credit cards, PayPal, and bank transfers.', 
+      isOpen: false },
+    { id: 2, 
+      question: 'How can I return or exchange an item?', 
+      answer: 'To return or exchange an item, please visit our return policy section for detailed instructions.', 
+      isOpen: false },
+  ]);
 
-    const toggleAnswer = (index) => {
-        if (openIndex === index) {
-            setOpenIndex(null); 
-        } else {
-            setOpenIndex(index); 
-        }
-    };
+  const toggleAnswer = (index: number) => {
+    setFaqItems(prevItems => {
+      const updatedItems = [...prevItems];
 
-    return (
-        <>
-            <div className="faq-container" id="faq">
-                <div className="faq-box">
-                    <h2 className="faq-title">FAQ</h2>
-                    <ul className="faq-content">
-                        <li className="faq-item">
-                            <p className='faq-question' onClick={() => toggleAnswer(0)}>
-                                How can I track the status of my order?
-                            </p>
-                            <div 
-                                className={`faq-button-show ${openIndex === 0 ? 'button-show' : ''}`} 
-                                onClick={() => toggleAnswer(0)}
-                            ></div>
-                            {openIndex === 0 && (
-                                <p className='faq-answer'>After placing your order, you will receive
-                                    a confirmation email containing your order number and a tracking link. 
-                                    You can also log in to your account on our website and go to the "My Orders" 
-                                    section to track your delivery status.
-                                </p>
-                            )}
-                        </li>
-                        <li className="faq-item">
-                            <p className='faq-question' onClick={() => toggleAnswer(1)}>
-                                What payment methods do you accept?
-                            </p>
-                            <div 
-                                className={`faq-button-show ${openIndex === 1 ? 'button-show' : ''}`} 
-                                onClick={() => toggleAnswer(1)}
-                            ></div>
-                            {openIndex === 1 && (
-                                <p className='faq-answer'>We accept various payment methods including credit cards, 
-                                    PayPal, and bank transfers.
-                                </p>
-                            )}
-                        </li>
-                        <li className="faq-item">
-                            <p className='faq-question' onClick={() => toggleAnswer(2)}>
-                                How can I return or exchange an item?
-                            </p>
-                            <div 
-                                className={`faq-button-show ${openIndex === 2 ? 'button-show' : ''}`} 
-                                onClick={() => toggleAnswer(2)}
-                            ></div>
-                            {openIndex === 2 && (
-                                <p className='faq-answer'>To return or exchange an item, please visit our return policy 
-                                    section for detailed instructions.
-                                </p>
-                            )}
-                        </li>
-                    </ul>
-                </div>   
-            </div>
-        </>
-    );
+      updatedItems[index].isOpen = !updatedItems[index].isOpen;
+      console.log(updatedItems[index].isOpen)
+
+      return updatedItems;
+    });
+  };
+
+
+  return (
+    <>
+      <div className="faq-container" id="faq">
+        <div className="faq-box">
+          <h2 className="faq-title">FAQ</h2>
+          <ul className="faq-content">
+            {faqItems.map((item, index) => (
+              <li key={item.id} className="faq-item">
+                <p className='faq-question'>{item.question}</p>
+                <button aria-label='show'
+                  className={`faq-button-show ${item.isOpen ? 'button-show' : ''}`} 
+                  onClick={() => toggleAnswer(index)} 
+                ></button>                
+                  <p className={`faq-answer ${item.isOpen ? 'button-show' : ''}`}>{item.answer}</p>
+                
+              </li>
+            ))}
+          </ul>
+        </div>   
+      </div>
+    </>
+  );
 }
 
 export default FAQ;
---
+++ src/share/ui/ButtonCount.tsx
@@ -0,0 +1,11 @@
+
+
+function ButtonCount() {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default ButtonCount
