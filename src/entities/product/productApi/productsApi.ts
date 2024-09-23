f385f5c97b5da94706291d7e312c390136e0a222
Mikhail2107 <mixa.proshin@yandex.ru>
2024-09-24 00:23:58 +0300

    gone to dev1

--- src/entities/product/productApi/productsApi.ts
+++ src/entities/product/productApi/productsApi.ts
@@ -1,6 +1,26 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { Product, ProductData } from './interface';
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+  returnPolicy: string;
+  warrantyInformation: string;
 
+}
+export interface ProductData {
+  products: Product[];
+  total: number;
+  limit: number;
+  skip: number;
+}
 export const productsApi = createApi({
   reducerPath: 'productsApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://dummyjson.com/' }), 
